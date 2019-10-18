041f1f6ba2526888a9f7aa9bf9c3326eea5bc87d
1070446210 <1070446210@qq.com>
2019-10-18 09:50:44 +0800

    打包问题

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,7 @@
 // vue.config.js
 module.exports = {
-  /* 部署生产环境和开发环境下的URL：可对当前环境进行区分，baseUrl 从 Vue CLI 3.3 起已弃用，要使用publicPath */
-  /* baseUrl: process.env.NODE_ENV === 'production' ? './' : '/' */
-  publicPath: process.env.NODE_ENV === 'production' ? '/public/' : './',
-  /* 输出文件目录：在npm run build时，生成文件的目录名称 */
-  outputDir: 'dist',
-  /* 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录 */
-  assetsDir: 'assets',
-  /* 是否在构建生产包时生成 sourceMap 文件，false将提高构建速度 */
-  productionSourceMap: false,
-  /* 默认情况下，生成的静态资源在它们的文件名中包含了 hash 以便更好的控制缓存，你可以通过将这个选项设为 false 来关闭文件名哈希。(false的时候就是让原来的文件名不改变) */
-  filenameHashing: true,
+  outputDir: 'toot',
+  publicPath: './',
   /* 代码保存时进行eslint检测 */
   lintOnSave: true,
   /* webpack-dev-server 相关配置 */
@@ -31,13 +22,6 @@ module.exports = {
     //     changeOrigin: true
     //   }
     // }
-  },
-  configureWebpack: config => {
-    if (process.env.NODE_ENV === 'production') {
-      // 为生产环境修改配置...
-    } else {
-      // 为开发环境修改配置...
-    }
   },
   chainWebpack: config => {
     // 一个规则里的 基础Loader
