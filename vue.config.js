8bf11beaeaac766e1ea2957bf1682a8ef3eaaf9b
unknown <1070446210.com>
2019-10-19 11:11:19 +0800

    config配置

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,10 @@ module.exports = {
   publicPath: './',
   /* 代码保存时进行eslint检测 */
   lintOnSave: true,
+  /* 是否在构建生产包时生成 sourceMap 文件，false将提高构建速度 */
+  productionSourceMap: false,
+  /* 默认情况下，生成的静态资源在它们的文件名中包含了 hash 以便更好的控制缓存，你可以通过将这个选项设为 false 来关闭文件名哈希。(false的时候就是让原来的文件名不改变) */
+  filenameHashing: true,
   /* webpack-dev-server 相关配置 */
   devServer: {
     /* 自动打开浏览器 */
